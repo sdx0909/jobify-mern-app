da63d78d5bbe94ccfab0a29d3ea80d84ac1d875a
sdx0909 <dixitsaurabh092@gmail.com>
2025-05-08 21:44:33 +0530

    done login-page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,28 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import { FormRow, Logo } from "../components";
 function Login() {
   return (
-    <div>
-      <h1>Login </h1>
-      {/* Link is worked with only page */}
-      <Link to="/register">Register Page</Link>
-    </div>
-  )
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Login Here</h4>
+        <FormRow type="email" name="email" defaultValue="john@gmail.com" />
+        <FormRow type="password" name="password" defaultValue="secret123" />
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+        <button type="button" className="btn btn-block">
+          explore the app
+        </button>
+        <p>
+          Not a member yet?
+          <Link to="/register" className="member-btn">
+            Register
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
+  );
 }
-export default Login
+export default Login;
