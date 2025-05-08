e8cafa7cd469f111d96c369b2e5f566fe9677430
sdx0909 <dixitsaurabh092@gmail.com>
2025-05-08 14:12:41 +0530

    adding the Wrapper-styling

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,19 +1,36 @@
 import styled from "styled-components"
 
 // creating the styled-button
-const StyledBtn = styled.button`
-  font-size: 1.5rem;
-  background: red;
-  color: white;
-`;
+// const StyledBtn = styled.button`
+//   font-size: 1.5rem;
+//   background: red;
+//   color: white;
+// `;
 
 function Landing() {
   return (
-    <div>
+    <Wrapper>
       <h1>Landing Page</h1>
+      <div className="content">some content</div>
       {/* attaching the styled-button */}
-      <StyledBtn>styled-btn</StyledBtn>
-    </div>
+      {/* <StyledBtn>styled-btn</StyledBtn> */}
+    </Wrapper>
   )
 }
+
+// using Wrapper for better styling 
+const Wrapper = styled.div`
+  background: red;
+
+  h1 {
+    color: white;
+  }
+
+  .content {
+    background-color: blue;
+    color: yellow;
+  }
+`
+
+
 export default Landing
